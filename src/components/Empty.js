39b8d2c5723ff96281e2217aa4ed8e00dc63f32c
Shruti_Ptl <shruti.p@brilworks.com>
2021-10-01 13:38:26 +0530

    add: project created

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -1,23 +1,25 @@
 import React from "react";
-import {
-    Paper,
-    Container,
-    Typography,
-  } from "@material-ui/core";
-  import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-  import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
+import { Paper, Container, Typography } from "@material-ui/core";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 
-const Empty = ({label,component})=>{
-    return(
-        <div style={{marginTop:"15px"}}>
-            <Container>
-                <Paper>
-                    {component==="cart"?<RemoveShoppingCartIcon className="empty-cart-icon"/>:<ShoppingBasketIcon className="empty-cart-icon"/>}
-                    <Typography variant="h4" align="center">{label}!!</Typography>
-                </Paper>
-            </Container>
-        </div>
-    )
-}
+const Empty = ({ label, component }) => {
+  return (
+    <div style={{ marginTop: "15px" }}>
+      <Container>
+        <Paper>
+          {component === "cart" ? (
+            <RemoveShoppingCartIcon className="empty-cart-icon" />
+          ) : (
+            <ShoppingBasketIcon className="empty-cart-icon" />
+          )}
+          <Typography variant="h4" align="center">
+            {label}!!
+          </Typography>
+        </Paper>
+      </Container>
+    </div>
+  );
+};
 
 export default Empty;
